5652f6e8a8748112339b5e067323252d490d1d6c
jelyqs <jely.big@gmail.com>
2025-10-03 11:34:24 +0700

    Init code

--- next.config.ts
+++ next.config.ts
@@ -150,6 +150,15 @@ const nextConfig: NextConfig = {
         ],
       },
       {
+        source: '/icon-:size*.png',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
         source: '/favicon.ico',
         headers: [
           {
